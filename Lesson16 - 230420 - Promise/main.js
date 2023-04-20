69b0033f83afb8355a38b3f318fce0ee6e9e6ed9
zoharelias <zoharelias@gmail.com>
2023-04-20 22:56:56 +0300

    promises - excercises without pagination

---
+++ Lesson16 - 230420 - Promise/main.js
@@ -0,0 +1,176 @@
+const btn0 = document.querySelector('.btn0');
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+const btn3 = document.querySelector('.btn3');
+const ex0userDiv = document.querySelector('.excercise0user');
+const ex1TodosDiv = document.querySelector('.excercise1Totos');
+const ex2UsersDiv = document.querySelector('.excercise2Users');
+const ex3catsDiv = document.querySelector('.excercise3cats');
+const ex4catsDiv = document.querySelector('.excercise4cats');
+const dropDown = document.querySelector('.select');
+
+//Ecercise 0
+const getUser = async () =>{
+  try {
+    const response = await fetch('https://randomuser.me/api/');
+    const data = await response.json();
+    const res = data.results[0];
+    let dataToShow = '';
+    dataToShow = `${res.name.first} ${res.name.last} from ${res.location.city}, ${res.location.country}, ${res.dob.age} years old. `;
+    ex0userDiv.innerHTML = dataToShow;
+
+  } catch(err){
+    console.log(err);
+  }
+}
+
+btn0.addEventListener('click', getUser);
+
+
+//Excercise 1
+const getTodos = async () =>{
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    const data = await response.json();
+    let dataToShow = '';
+    ex1TodosDiv.innerHTML = '';
+    console.log(data);
+    const ul = document.createElement('ul');
+    data.forEach(element => {
+        const li = document.createElement('li');
+        li.innerText = element.title;
+        if(element.completed){
+          li.classList.add('done');
+        }
+        ul.append(li);
+    });
+    ex1TodosDiv.append(ul);  
+  } catch(err){
+    console.log(err);
+  }
+}
+
+btn1.addEventListener('click', getTodos);
+
+
+//Ecercise 2
+const getUsers = async () =>{
+  try {
+    const response = await fetch('https://reqres.in/api/users');
+    const data = await response.json();
+    const res = data.data;
+    let dataToShow = '';
+    ex2UsersDiv.innerHTML = '';
+    console.log(data);
+    console.log(res);
+    const usersDiv = document.createElement('div');
+    usersDiv.classList.add('excercise2Users');
+    res.forEach(user=>{
+      const userDiv = document.createElement('div');
+      const userImg = document.createElement('img');
+      userImg.width = userImg.height = 100;
+      userImg.src = user.avatar;
+      dataToShow = `<div>${user.first_name} ${user.last_name}</div>`;
+      console.log(dataToShow);
+      userDiv.innerHTML= dataToShow;
+      userDiv.append(userImg);
+      usersDiv.append(userDiv);
+    });
+    ex2UsersDiv.append(usersDiv);
+  } catch(err){
+    console.log(err);
+  }
+}
+
+btn2.addEventListener('click', getUsers);
+
+//Ecercise 3
+const getCatImages = async () =>{
+  try {
+    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+    const data = await response.json();
+    const res = data.data;
+    let dataToShow = '';
+    ex3catsDiv.innerHTML = '';
+    console.log(data);
+    console.log(res);
+    const usersDiv = document.createElement('div');
+    usersDiv.classList.add('excercise2Users');
+    data.forEach(cat=>{
+      const userImg = document.createElement('img');
+      userImg.width = userImg.height = 100;
+      userImg.src = cat.url;
+      usersDiv.append(userImg);
+    });
+    ex3catsDiv.append(usersDiv);
+  } catch(err){
+    console.log(err);
+  }
+}
+
+btn3.addEventListener('click', getCatImages);
+
+
+//Ecercise 4
+const getCatBreeds = async () =>{
+  try {
+    const response = await fetch('https://api.thecatapi.com/v1/breeds');
+    const data = await response.json();
+    let dataToShow = '';
+    console.log(data);
+    data.forEach(cat=>{
+      const option = document.createElement('option');
+      option.value = cat.id;
+      option.text = cat.name;
+      dropDown.append(option);
+
+    })
+    // const usersDiv = document.createElement('div');
+    // usersDiv.classList.add('excercise2Users');
+    // data.forEach(cat=>{
+    //   const userImg = document.createElement('img');
+    //   userImg.width = userImg.height = 100;
+    //   userImg.src = cat.url;
+    //   usersDiv.append(userImg);
+    // ex3catsDiv.append(usersDiv);
+  } catch(err){
+    console.log(err);
+  }
+}
+
+
+const getCatBreed = async (breed) =>{
+  try {
+    const response = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breed}`);
+    const data = await response.json();
+    let dataToShow = '';
+    ex4catsDiv.innerHTML = '';
+    console.log(data);
+    // const usersDiv = document.createElement('div');
+    // usersDiv.classList.add('excercise2Users');
+    // data.forEach(cat=>{
+    const catImg = document.createElement('img');
+    catImg.width = catImg.height = 100;
+    catImg.src = data[0].url;
+    //   usersDiv.append(userImg);
+    ex4catsDiv.append(catImg);
+  } catch(err){
+    console.log(err);
+  }
+}
+
+
+
+
+getCatBreeds();
+
+dropDown.addEventListener("change", (option) => {
+  const catBreed = option.target.value;
+  console.log(catBreed);
+  getCatBreed(catBreed);
+});
+
+// selectElement.addEventListener("change", (event) => {
+//   const result = document.querySelector(".result");
+//   result.textContent = `You like ${event.target.value}`;
+// });
