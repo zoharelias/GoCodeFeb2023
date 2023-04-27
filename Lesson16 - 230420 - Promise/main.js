a2066e44c2487895a9fb690439bccce35cb59cf3
zoharelias <zoharelias@gmail.com>
2023-04-27 22:59:04 +0300

    React

--- Lesson16 - 230420 - Promise/main.js
+++ Lesson16 - 230420 - Promise/main.js
@@ -1,13 +1,18 @@
 const btn0 = document.querySelector('.btn0');
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
-const btn3 = document.querySelector('.btn3');
+// const btn3 = document.querySelector('.btn3');
+// const btn32 = document.querySelector('.btn32');
+const btnPrev = document.querySelector('.prev');
+const btnNext = document.querySelector('.next');
 const ex0userDiv = document.querySelector('.excercise0user');
 const ex1TodosDiv = document.querySelector('.excercise1Totos');
 const ex2UsersDiv = document.querySelector('.excercise2Users');
 const ex3catsDiv = document.querySelector('.excercise3cats');
 const ex4catsDiv = document.querySelector('.excercise4cats');
 const dropDown = document.querySelector('.select');
+const pageNumDiv = document.querySelector('.pageNum');
+let gPageNum = 1; //global
 
 //Ecercise 0
 const getUser = async () =>{
@@ -70,7 +75,7 @@ const getUsers = async () =>{
       const userImg = document.createElement('img');
       userImg.width = userImg.height = 100;
       userImg.src = user.avatar;
-      dataToShow = `<div>${user.first_name} ${user.last_name}</div>`;
+      dataToShow = `<div>${user.first_name} ${user.last_name} ${user.email}</div>`;
       console.log(dataToShow);
       userDiv.innerHTML= dataToShow;
       userDiv.append(userImg);
@@ -85,9 +90,19 @@ const getUsers = async () =>{
 btn2.addEventListener('click', getUsers);
 
 //Ecercise 3
-const getCatImages = async () =>{
+const getCatImages = async (pageNum) =>{
   try {
-    const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=10');
+    console.log('excercise 3:');
+    console.log(pageNum);
+    let myParams = new URLSearchParams();
+    //params.
+    const url = `https://api.thecatapi.com/v1/images/search?limit=10&page=${pageNum}`
+    console.log(url);
+    const response = await fetch(url);
+    pageNumDiv.innerText = pageNum;
+    // let params = new URLSearchParams(document.location.search);
+    // let name = params.get("limit"); // is the string "Jonathan"
+    // 
     const data = await response.json();
     const res = data.data;
     let dataToShow = '';
@@ -108,7 +123,22 @@ const getCatImages = async () =>{
   }
 }
 
-btn3.addEventListener('click', getCatImages);
+btnPrev.addEventListener('click', ()=>{
+  if(gPageNum === 1){
+    gPageNum =1;
+  } else {
+    gPageNum--;
+  }
+  getCatImages(gPageNum);
+});
+
+btnNext.addEventListener('click', ()=>{
+    gPageNum++;
+  getCatImages(gPageNum);
+});
+
+//btn3.addEventListener('click', ()=>{getCatImages(1)});
+//btn32.addEventListener('click', ()=>{getCatImages(2)});
 
 
 //Ecercise 4
